150cc758c6dc981482af4ea16313e58c39dc3e10
deki84 <deki1984@hotmail.de>
2025-07-14 22:54:51 +0200

    add Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './globals.css';
+import { Metadata } from 'next'
+
+// SEO-Metadaten
+export const metadata: Metadata = {
+  title: 'Buchhaltung in Augsburg – Professionell & digital',
+  description: 'Professionelle Buchhaltung – digital, sorgfältig und persönlich. Jetzt informieren!',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
