fba8a6cef29fefdd4232148f496820f83e9741b1
deki84 <deki1984@hotmail.de>
2026-02-24 18:18:22 +0100

    fix: update Next.js, change copyright year to 2026

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-blue-800 text-white text-xs sm:text-sm py-4 sm:py-6 font-[Cambria] shadow-inner">
       <div className="max-w-screen-md mx-auto px-4 text-center space-y-2">
-        <p>&copy; 2025 buchhalt.de. Alle Rechte vorbehalten.</p>
+        <p>&copy; 2026 buchhalt.de. Alle Rechte vorbehalten.</p>
         <nav>
           <ul className="flex flex-col sm:flex-row justify-center items-center gap-1 sm:gap-x-6">
             <li>
